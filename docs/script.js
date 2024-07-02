9e056ac456fac1a19bb164adc862842e00780b59
Orl4NdZ <147104728+Orl4NdZ@users.noreply.github.com>
2024-07-02 23:08:29 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -118,8 +118,10 @@ function animate() {
 animate();
 
 // Resize handler
-window.addEventListener('resize', () => {
+function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
-});
+}
+
+window.addEventListener('resize', onWindowResize);
