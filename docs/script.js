2d80f4f47d09dffbc42de23a741e05dffda2b5d8
Orl4NdZ <147104728+Orl4NdZ@users.noreply.github.com>
2025-03-13 16:42:55 +0100

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,13 +1,22 @@
+const ctx = document.createElement('canvas').getContext('2d')
+const gradient = ctx.createLinearGradient(0, 0, 0, 150)
+gradient.addColorStop(0, 'rgb(200, 0, 200)')
+gradient.addColorStop(0.7, 'rgb(100, 0, 100)')
+gradient.addColorStop(1, 'rgb(0, 0, 0)')
+
+
+
+
 var wavesurfer = WaveSurfer.create({
     container: "#audiowave",
-    waveColor: "#5df9de",
+    waveColor: gradient,
     progressColor: "#1e594f",
     height: 150,
     responsive: true,
     hideScrollbar: true,
     cursorColor: "#5df9de",
     cursorWidth: 2,
-    barWidth: 5,
+    barWidth: 3,
     barGap: 1.5,
     skipLength: 5
 });
